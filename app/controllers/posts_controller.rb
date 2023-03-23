105f88e2c1e036135de73b17a46b844425998110
Happiness-James <gesarehjames@gmail.com>
2023-03-23 13:49:42 +0000

    assignment done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+render_from ApplicationRecord::RecordInvalid with: :render_unprocessable_entity_response
+
 
   def show
     post = Post.find(params[:id])
@@ -9,7 +11,7 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +20,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+  def render_unprocessable_entity_response(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
 end
